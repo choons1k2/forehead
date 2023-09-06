efce708a1210afb6f18e2d534c61ce6e2736ae9e
choonsik <108234111+choons1k2@users.noreply.github.com>
2023-09-06 11:43:59 +0900

    crop forehead complete

--- forehead_pigmentation_classification.py
+++ forehead_pigmentation_classification.py
@@ -1,5 +1,6 @@
 import os
 import json
+from PIL import Image
 
 folder_format = '{:04d}'
 
@@ -23,10 +24,33 @@ for iter in range (1, 100 + 1):
         
         #forehead bbox point 
         for key, value in value.items():
-            if key=='bbox':
+            if key == 'bbox':
                 forehead.append(value)
                 print(value)
-                
+    
+    crop = forehead[iter - 1]
+    x1, y1 = crop[0], crop[1]
+    x2, y2 = crop[2], crop[3]
+    
+    img_path = os.path.join(r'C:\Users\MLPA\Desktop\NIA\NIA_num_100_data\masking\01', 
+                              folder_name, f'{folder_name}_01_F.jpg')
+    
+    img = Image.open(img_path)
+    
+    cropped_img = img.crop((x1, y1, x2, y2))
+    
+    output_path = os.path.join(r'C:\Users\MLPA\Desktop\NIA\NIA_num_100_data\masking\01', 
+                              folder_name, 'cropped_img')
+    os.makedirs(output_path, exist_ok=True)
+    
+    output_path = os.path.join(output_path, f'{folder_name}_01_F_forehead.jpg')
+    
+    
+    cropped_img.save(output_path)
 
-print(forehead)   
-                
+   
+    
+             
+    
+    
+    
